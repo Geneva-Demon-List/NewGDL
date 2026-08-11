7537688781e52d3856ec1f829ec4c9caab4029f0
Rocketpowered-tech <chaserob12345@gmail.com>
2026-08-11 00:26:26 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,9 +11,6 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 9999) {
-        return 0;
-    }
     if (rank > 75 && percent < 100) {
         return 0;
     }
@@ -45,8 +42,8 @@ export function score(rank, percent, minPercent) {
     
     if (percent != 100) {
         let waythroughthelevel = ((percent - minPercent) / (100 - minPercent));
-        score = ((score / 5) * (waythroughthelevel * 3) + (score / 5));
-        return score;
+        let newscore = ((score / 5) * (waythroughthelevel * 3) + (score / 5));
+        return newscore;
     }
     
     return Math.max(round(score), 0);
